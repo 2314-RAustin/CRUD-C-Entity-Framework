b0a7acc687b37777d41fa23a97d9bc0ac31a03dd
Medina Austin Roberto Eduardo <0315108515@miutt.edu.mx>
2020-06-23 11:23:23 -0700

    Base crud with entity framwork

---
+++ RobertoCRUD/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RobertoCRUD.Models;
+
+namespace RobertoCRUD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            refreshData();
+        }
+
+        private void refreshData()
+        {
+            using (var db = new CrudEntities())
+            {
+                var list = from d in db.datos_users
+                           select d;
+                dataGridView1.DataSource = list.ToList();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Presentational.Form1 F1 = new Presentational.Form1();
+            F1.ShowDialog();
+
+            refreshData();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int? id = getId();
+            if (id != null)
+            {
+                Presentational.Form1 F1 = new Presentational.Form1(id);
+                F1.ShowDialog();
+
+                refreshData();
+            }
+
+        }
+
+        private int? getId()
+        {
+            try
+            {
+                return int.Parse(dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[0].Value.ToString());
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int? id = getId();
+            if (id != null)
+            {
+                using (CrudEntities db = new CrudEntities())
+                {
+                    datos_users dataUsers = db.datos_users.Find(id);
+                    db.datos_users.Remove(dataUsers);
+                }
+                
+                refreshData();
+            }
+        }
+    }
+}
---
+++ RobertoCRUD/Presentational/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RobertoCRUD.Models;
+
+namespace RobertoCRUD.Presentational
+{
+    public partial class Form1 : Form
+    {
+        private int? id;
+        private datos_users _dataUser = null;
+
+        public Form1(int? id=null)
+        {
+            InitializeComponent();
+
+            this.id = id;
+
+            if (id != null)
+            {
+                loadData();
+            }
+        }
+
+        private void loadData()
+        {
+            using (var db = new CrudEntities())
+            {
+                _dataUser = db.datos_users.Find(id);
+                txtName.Text = _dataUser.nombre;
+                txtEmail.Text = _dataUser.correo;
+                dtpBirth.Value = _dataUser.fecha_nacimiento;
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var db = new CrudEntities())
+            {
+                if (id == null)
+                {
+                    _dataUser = new datos_users();    
+                }
+
+                _dataUser.nombre = txtName.Text;
+                _dataUser.correo = txtEmail.Text;
+                _dataUser.fecha_nacimiento = dtpBirth.Value;
+
+                if (id == null)
+                {
+                    db.datos_users.Add(_dataUser);
+                }
+                else
+                {
+                    db.Entry(_dataUser).State = System.Data.Entity.EntityState.Modified;
+                }
+                
+                db.SaveChanges();
+
+                this.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
